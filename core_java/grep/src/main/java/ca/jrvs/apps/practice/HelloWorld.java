9d96773c6d7cc9dd1fcd285b452d495c74b0aa5b
Mohammad Asif <mohammadhamza.asif@ontariotechu.net>
2024-11-01 14:25:58 +0000

    Grep Basic Implementation

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -1,10 +1,21 @@
 package ca.jrvs.apps.practice;
 
-class HelloWorld {
+//class HelloWorld {
+//
+//  // Your program begins with a call to main().
+//  // Prints "Hello, World" to the terminal window.
+//  public static void main(String[] args) {
+//    System.out.println("Hello, World");
+//  }
+//}
 
-  // Your program begins with a call to main().
-  // Prints "Hello, World" to the terminal window.
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+    logger.info("Hello World");
   }
 }
